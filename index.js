40337f1cefb8fe7d579d6d224e9fe42ec27c4ead
Matthew Trabold <mct4240@gmail.com>
2019-01-15 21:04:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,8 @@ function destructivelyRemoveLastKitten(name) {
   kittens.pop(name);
   return kittens;
 }
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name);
+  return kittens;
+}
